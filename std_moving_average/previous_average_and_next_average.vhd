4c00c399ffca15f879246ba178ead2cf5c6f8d05
xiaohezhe <1569529820@qq.com>
2021-02-10 19:20:31 +0100

    get deviation(middle data subtract average value)

--- std_moving_average/previous_average_and_next_average.vhd
+++ std_moving_average/previous_average_and_next_average.vhd
@@ -10,22 +10,28 @@ generic (
 port (
 	  i_clk                      : in  std_logic;
   	  i_data                     : in  std_logic_vector(data_bits-1 downto 0);
-          o_data_next                : out std_logic_vector(data_bits-1 downto 0);--average data for next group
-          o_data_previous	     : out std_logic_vector(data_bits-1 downto 0);--average data for previous group
 	  o_data_average	     : out std_logic_vector(data_bits-1 downto 0);
-	  o_data_subtract	     : out std_logic_vector(data_bits-1 downto 0)
+	  o_data_subtract	     : out std_logic_vector(data_bits-1 downto 0);
+	  o_data_fraction	     : out std_logic_vector(group_length downto 0)
 	);
 end previous_average_and_next_average;
 
 architecture arc_previous_average_and_next_average of previous_average_and_next_average is
 signal oldest_data_reg_vector:std_logic_vector(data_bits-1 downto 0):=(others=>'0');
 signal oldest_data_reg_signed: signed(data_bits-1 downto 0):=(others=>'0');
+
+
 signal oldest_data_pre: std_logic_vector(data_bits-1 downto 0):=(others=>'0');
-signal o_data_next_reg: std_logic_vector(data_bits-1 downto 0):=(others=>'0');
-signal o_data_previous_reg: std_logic_vector(data_bits-1 downto 0):=(others=>'0');
-signal sum_reg: signed(data_bits-1 downto 0):=(others => '0');
+
+--sum value of previous group and next group
+signal o_data_next_reg: std_logic_vector(data_bits+group_length downto 0):=(others=>'0');
+signal o_data_previous_reg: std_logic_vector(data_bits+group_length downto 0):=(others=>'0');
+
+
+signal sum_reg: signed(data_bits+group_length downto 0):=(others => '0');
 signal average_reg: signed(data_bits-1 downto 0):=(others=>'0');--9 bits
 signal subtract_reg:signed(data_bits-1 downto 0):=(others => '0');
+signal fraction_reg: signed(group_length downto 0):=(others => '0');
 
 component moving_average
 generic (
@@ -36,7 +42,7 @@ port (
   -- input
   i_data                     : in  std_logic_vector(data_bits-1 downto 0);
   -- output
-  o_data                     : out std_logic_vector(data_bits-1 downto 0);
+  o_data                     : out std_logic_vector(data_bits+group_length downto 0);
   oldest_data		     : out std_logic_vector(data_bits-1 downto 0));
 end component moving_average;
 
@@ -60,19 +66,10 @@ port map(
 
 oldest_data_reg_signed <= signed(oldest_data_reg_vector);
 
-total_average : process(i_clk)
-begin
- if(rising_edge(i_clk)) then
-	sum_reg <= signed(o_data_previous_reg)+signed(o_data_next_reg);
-	average_reg<= '0'&signed(sum_reg(data_bits-1 downto 1)); --divide by 2
-	subtract_reg<= signed(oldest_data_reg_signed-average_reg);--midddle data-average data
-end if;
-end process total_average;
-o_data_average <= std_logic_vector(average_reg(data_bits+group_length-3 downto 0));
-o_data_subtract <= std_logic_vector(subtract_reg);
-o_data_previous <= o_data_previous_reg;
-o_data_next<= o_data_next_reg;
 
-
+sum_reg <= signed(o_data_previous_reg)+signed(o_data_next_reg);
+o_data_average <= std_logic_vector(sum_reg(data_bits+group_length downto (group_length+1)));
+o_data_subtract <=std_logic_vector(signed(oldest_data_reg_signed-signed(sum_reg(data_bits+group_length downto (group_length+1)))));
+o_data_fraction<=std_logic_vector(sum_reg(group_length downto 0));
 
 end arc_previous_average_and_next_average;
